0f5a7a146dfbae587f1c01decfff44596c38f5f4
PetitPenguin <ra1issa@bu.edu>
2019-06-30 16:58:10 -0400

    Add ability to parse share and open in Babel.

--- main.js
+++ main.js
@@ -29,75 +29,96 @@ window.onload = function() {
 window.spec = {};
 
 function createMetric(spec) {
-return function () {
-  var zero = polynomium.c(0).toObject(),
-      one = polynomium.c(1).toObject(),
-      plus = function (sum, node) { return polynomium.add(sum, node.metric).toObject(); };
+  return function () {
+    var zero = polynomium.c(0).toObject(),
+        one = polynomium.c(1).toObject(),
+        plus = function (sum, node) { return polynomium.add(sum, node.metric).toObject(); };
 
-  return carousels.babelVisitorDefaults({
-    visitor: {
-      Program: {
-        "exit": function (p) {
+    return carousels.babelVisitorDefaults({
+      visitor: {
+        Program: {
+          "exit": function (p) {
 
-          var results = {}, metric = {};
-          for (var i = 0; i < p.node.body.length; i++) {
-            metric[p.node.body[i].id.name] = p.node.body[i].metric;
-            results[p.node.body[i].id.name] = polynomium.toString(p.node.body[i].metric);
+            var results = {}, metric = {};
+            for (var i = 0; i < p.node.body.length; i++) {
+              metric[p.node.body[i].id.name] = p.node.body[i].metric;
+              results[p.node.body[i].id.name] = polynomium.toString(p.node.body[i].metric);
+            }
+            p.node.metric = metric;
+            p.node.results = results;
           }
-
-          p.node.metric = metric;
-          p.node.results = results;
-        }
-      },
-      FunctionDeclaration: {
-        "exit": function (p) {
-          p.node.metric = p.node.body.metric;
-        }
-      },
-      Identifier: {
-        "exit": function (p) {
-          p.node.metric = zero;
-        }
-      },
-      BlockStatement: {
-        "exit": function (p) {
-          p.node.metric = p.node.body.reduce(plus, zero);
-        }
-      },
-      VariableDeclaration: {
-        "exit": function (p) {
-          p.node.metric = p.node.declarations.reduce(plus, zero);
-        }
-      },
-      VariableDeclarator: {
-        "exit": function (p) {
-          p.node.metric = p.node.init.metric;
-        }
-      },
-      ReturnStatement: {
-        "exit": function (p) {
-          p.node.metric = p.node.argument.metric;
-        }
-      },
-      BinaryExpression: {
-        "exit": function (p) {
-          var start = p.node.loc.start, op = p.node.operator;
-          if (op in spec) {
-            p.node.metric = [p.node.left, p.node.right].reduce(plus, spec[op]);
-          } else {
-            throw Error("Node type BinaryExpression with operator " + op +
-                        " is not handled at line " + start.line + ", column " + start.column + ".");
+        },
+        ExpressionStatement: {
+          "exit": function (p) {
+            p.node.metric = p.node.expression.metric;
           }
-        }
-      },
-      NumericLiteral: {
-        "exit": function (p) {
-          p.node.metric = zero;
+        },
+        CallExpression: {
+          "exit": function (p) {
+            console.log("CallExpression");
+
+            var start = p.node.loc.start;
+            var op_name = p.node.callee.name;
+            var arguments = p.node.arguments;
+
+            if (op_name in spec) {
+              p.node.metric = arguments.reduce(plus, spec[op_name]);
+            } else {
+              throw Error("Node type CallExpression with operator " + op +
+                          " is not handled at line " + start.line + ", column " + start.column + ".");
+            }
+          }
+        },
+        FunctionDeclaration: {
+          "exit": function (p) {
+            p.node.metric = p.node.body.metric;
+            console.log(p.node.metric );
+          }
+        },
+        Identifier: {
+          "exit": function (p) {
+            p.node.metric = zero;
+          }
+        },
+        BlockStatement: {
+          "exit": function (p) {
+            p.node.metric = p.node.body.reduce(plus, zero);
+          }
+        },
+        VariableDeclaration: {
+          "exit": function (p) {
+            p.node.metric = p.node.declarations.reduce(plus, zero);
+          }
+        },
+        VariableDeclarator: {
+          "exit": function (p) {
+            p.node.metric = p.node.init.metric;
+          }
+        },
+        ReturnStatement: {
+          "exit": function (p) {
+            p.node.metric = p.node.argument.metric;
+          }
+        },
+        BinaryExpression: {
+          "exit": function (p) {
+            var start = p.node.loc.start, op = p.node.operator;
+            if (op in spec) {
+              p.node.metric = [p.node.left, p.node.right].reduce(plus, spec[op]);
+            } else {
+              throw Error("Node type BinaryExpression with operator " + op +
+                          " is not handled at line " + start.line + ", column " + start.column + ".");
+            }
+          }
+        },
+        NumericLiteral: {
+          "exit": function (p) {
+            p.node.metric = zero;
+          }
         }
       }
-    }
-  });
-};
+    });
+  };
 }
 Babel.registerPlugin('metric', createMetric(window.spec));
 
@@ -131,14 +152,17 @@ function demo(specText, inpText, outDiv) {
       var color_seed = 0;
       for (var name in bbl.ast.program.metric) {
         color_seed += 10;
+
         var p = bbl.ast.program.metric[name];
         var color = randomColor({"seed":color_seed});
         window.graph_config.data.datasets.push({
+
           label:name,
           backgroundColor:color,
           borderColor:color,
           data:ran.map(function (b) { return polynomium.evaluate(p, {"b":b}); }),
           fill:false
+
         });
       }
       document.getElementById('spec').value = JSON.stringify(onlineRounds, null, " ");
