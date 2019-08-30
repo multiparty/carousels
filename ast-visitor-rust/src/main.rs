589863268f549140fb6847b28e15836780077dcd
PetitPenguin <ra1issa@bu.edu>
2019-08-30 13:05:28 -0400

    Add struct supporting AST node in IR. Fill out more visitor patterns

--- ast-visitor-rust/src/main.rs
+++ ast-visitor-rust/src/main.rs
@@ -1,39 +1,96 @@
 use quote::quote;
 use syn::visit::{self, Visit};
-use syn::{File, ItemFn, FnDecl, Lit, ExprMethodCall,
+use syn::{File, ItemFn, FnDecl, Stmt, Lit, ExprMethodCall,
     ExprBinary, ExprLit, ExprCall, ExprClosure, ExprUnary,
-    ExprArray, ExprIndex, PatIdent, Member};
+    ExprArray, ExprIndex, PatIdent, Member, Pat, PatLit};
 
 fn main() {
     let src = "
             fn main() {
             let x = 5 + 2;
             let mut array: [i32; 3] = [0; 3];
-            let mut array2: [i32; 3] = [0, 0 ,0];
             }
         ";
     let syntax = syn::parse_file(&src).unwrap();
     let mut visitor = FnVisitor { ir: Vec::new() };
     visitor.visit_file(&syntax);
-    for lit in visitor.ir.iter() {
-        println!("{}", lit);
-    }
 }
+
+// this.id = id;
+// this.type = type;
+// this.body = body;
+// this.arg = arg;
+// this.start = start;
+// this.end = end;
+// this.parent = parent;
+// this.metric = metric;
+// this.results = results;
 
 struct FnVisitor {
-    ir: Vec<String>,
-
+    ir: Vec<Node>,
 }
 
-impl<'ast> Visit<'ast> for FnVisitor {
-    fn visit_item_fn(&mut self, node: &'ast ItemFn){
-        let fn_name = &node.ident;
-        let fn_body = &node.block.stmts;
-        self.ir.push(format!("{:#?}", fn_name));
+#[derive(Debug, Default)]
+struct Node{
+    id: String,
+    typ: String,
+    body: String,
+    arg: String,
+    parent: String,
+    value: String
+}
 
-        for stmt in fn_body {
+
+impl <'ast> Visit<'ast> for FnVisitor {
+    fn visit_item_fn(&mut self, node: &'ast ItemFn){
+        let fn_name = format!("{:#?}", &node.ident);
+        let fn_body = format!("{:#?}", &node.block.stmts);
+        let fn_node = Node {id: fn_name, typ: "function".to_string(), body:fn_body,
+                            arg: "".to_string(), parent: "".to_string(), value: "".to_string()};
+
+        println!("{}", format!("{:#?}", &node.block.stmts));
+        self.ir.push(fn_node);
+
+        for stmt in &node.block.stmts {
             self.visit_stmt(stmt);
         }
+    }
+    fn visit_stmt(&mut self, node: &'ast Stmt){
+        let mut stmt_node = Node::default();
+        match node {
+            Stmt::Local(_loc)=>{
+                let p = &_loc.pats[0];
+                let init = &_loc.init;
+                let ty = &_loc.ty;
+
+                match p {
+                    Pat::Ident(_p) =>{
+                        self.visit_pat_ident(&_p);
+                    }
+                    Pat::Lit(_l)=>{
+                        self.visit_pat_lit(&_l);
+                    }
+                    Pat::Tuple(_tpl)=>{
+                        self.visit_pat_tuple(&_tpl);
+                    }
+                    _=>{
+
+                    }
+                }
+
+
+            }
+            Stmt::Item(_item)=>{
+
+            }
+            Stmt::Expr(_expr)=>{
+
+            }
+            Stmt::Semi(_expr, _semi)=>{
+
+            }
+        }
+        self.ir.push(stmt_node);
     }
     fn visit_expr_call(&mut self, node: &'ast ExprCall){
     }
@@ -46,6 +103,7 @@ impl<'ast> Visit<'ast> for FnVisitor {
         let left = &node.left;
         let right = &node.right;
         let op = &node.op;
+
         self.visit_expr(&*node.left);
         self.visit_bin_op(&node.op);
         self.visit_expr(&*node.right);
