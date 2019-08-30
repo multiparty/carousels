83233be875375da6e10277fa979458ac340d8961
PetitPenguin <ra1issa@bu.edu>
2019-08-29 22:46:17 -0400

    Adding placeholders and accessing certain necessary nodes.

--- ast-visitor-rust/src/main.rs
+++ ast-visitor-rust/src/main.rs
@@ -1,6 +1,6 @@
 use quote::quote;
 use syn::visit::{self, Visit};
-use syn::{File, ItemFn, FnDecl, ExprMethodCall,
+use syn::{File, ItemFn, FnDecl, Lit, ExprMethodCall,
     ExprBinary, ExprLit, ExprCall, ExprClosure, ExprUnary,
     ExprArray, ExprIndex, PatIdent, Member};
 
@@ -8,6 +8,8 @@ fn main() {
     let src = "
             fn main() {
             let x = 5 + 2;
+            let mut array: [i32; 3] = [0; 3];
+            let mut array2: [i32; 3] = [0, 0 ,0];
             }
         ";
     let syntax = syn::parse_file(&src).unwrap();
@@ -24,41 +26,71 @@ struct FnVisitor {
 }
 
 impl<'ast> Visit<'ast> for FnVisitor {
-    fn visit_fn_decl(&mut self, node: &'ast FnDecl){
+    fn visit_item_fn(&mut self, node: &'ast ItemFn){
+        let fn_name = &node.ident;
+        let fn_body = &node.block.stmts;
+        self.ir.push(format!("{:#?}", fn_name));
 
+        for stmt in fn_body {
+            self.visit_stmt(stmt);
+        }
     }
     fn visit_expr_call(&mut self, node: &'ast ExprCall){
-
     }
     fn visit_expr_method_call(&mut self, node: &'ast ExprMethodCall){
-
     }
     fn visit_expr_closure(&mut self, node: &'ast ExprClosure){
 
     }
-    fn visit_expr_unary(&mut self, node: &'ast ExprUnary){
-
-    }
     fn visit_expr_binary(&mut self, node: &'ast ExprBinary) {
-          for attr in &node.attrs {
-              self.visit_attribute(attr);
-          }
-          self.visit_expr(&*node.left);
-          self.visit_bin_op(&node.op);
-          self.visit_expr(&*node.right);
+        let left = &node.left;
+        let right = &node.right;
+        let op = &node.op;
+        self.visit_expr(&*node.left);
+        self.visit_bin_op(&node.op);
+        self.visit_expr(&*node.right);
       }
-     fn visit_expr_lit(&mut self, node: &'ast ExprLit){
-          self.ir.push(format!("{:#?}", node.lit));
+     fn visit_lit(&mut self, node: &'ast Lit){
+         match node{
+            Lit::Str(_s) => {
+                let value = _s.value();
+            }
+            Lit::ByteStr (_bs) => {
+                let value = _bs.value();
+            }
+            Lit::Byte (_b) => {
+                let value = _b.value();
+            }
+            Lit::Char(_ch) =>{
+                let value = _ch.value();
+            }
+            Lit::Int(_i) => {
+                let value = _i.value();
+            }
+            Lit::Float(_f) =>{
+                let value = _f.value();
+            }
+            Lit::Bool(_bo) => {
+                let value = _bo.value;
+            }
+            _=>{
+
+            }
+         }
      }
      fn visit_expr_array(&mut self, node: &'ast ExprArray){
-
+         let elements = &node.elems;
      }
      fn visit_expr_index(&mut self, node: &'ast ExprIndex){
 
      }
      fn visit_pat_ident(&mut self, node: &'ast PatIdent){
-
+         let identifier = &node.ident;
      }
      fn visit_member(&mut self, node: &'ast Member){
      }
 }
+
+
+
+        // println!("{}",format!("{:#?}", fn_name));
