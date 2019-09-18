e5cbc9a993e7d55b897d33b6066dcf6267cf11f3
p-flock <pflock@bu.edu>
2019-09-18 14:17:37 -0400

    Demo of json traversal on rust ast - Toy example

--- achilles-ast/js/index.js
+++ achilles-ast/js/index.js
@@ -1,10 +1,32 @@
 // Module needs to be load dynamically (async) for WebAssembly to function
 import("../pkg/index.js").then(rust => {
-    document.getElementById('myButton').addEventListener('click', getAST);
+  document.getElementById('myButton').addEventListener('click', getAST);
+  document.getElementById('metric').addEventListener('click', buildPoly);
 
-    function getAST (e) {
-        e.preventDefault();
-        const res = rust.get_ast(document.getElementById('myTextarea').value);
-        document.getElementById('result').innerHTML  = JSON.stringify(JSON.parse(res), null, 2);;
+  function getAST (e) {
+    e.preventDefault();
+    const res = rust.get_ast(document.getElementById('myTextarea').value);
+    document.getElementById('result').innerHTML  = JSON.stringify(JSON.parse(res), null, 2);;
+  }
+
+  var metric = [];
+  function buildPoly(e) {
+    e.preventDefault();
+    var IR = document.getElementById('result').innerHTML;
+    IR = JSON.parse(IR);
+    traverse(IR)
+    document.getElementById('polynomial').innerHTML = metric;
+  }
+  function traverse(jsonObj) {
+    if( jsonObj !== null && typeof jsonObj == "object" ) {
+      Object.entries(jsonObj).forEach(([key, value]) => {
+        // key is either an array index or object key
+        traverse(value);
+      });
     }
+    else {
+      // jsonObj is a number or string
+      metric.push(jsonObj);
+    }
+  }
 }).catch(console.error);
