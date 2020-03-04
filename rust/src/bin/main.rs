3ea105e6cea8279d8eb7d86aab22bba952726f2a
p-flock <pflock@bu.edu>
2020-03-04 17:53:59 -0500

    remove extraneous logging / println statements

--- rust/src/bin/main.rs
+++ rust/src/bin/main.rs
@@ -11,11 +11,13 @@ use carousels::visitor;
 pub fn main() {
     let filename = "src/old/test_program.rs";
 
-    let file = visitor::get_ast(&filename);
-    match file {
-        Ok(_v)=>{println!("{}", serde_json::to_string(&_v).unwrap());},
-        Err(e) => println!("error parsing : {:?}", e),
-    };
+    /*
+     *let file = visitor::get_ast(&filename);
+     *match file {
+     *    Ok(_v)=>{println!("{}", serde_json::to_string(&_v).unwrap());},
+     *    Err(e) => println!("error parsing : {:?}", e),
+     *};
+     */
 
     let file_str = visitor::get_ast_str_from_file(&filename);
     match file_str {
