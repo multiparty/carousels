7d11f57ab865e8d93ca6a7b25de82eeb65b4fcd0
Wyatt Howe <wyatt@wyatthowe.net>
2020-03-05 02:34:58 -0500

    ui

--- docs/math.js
+++ docs/math.js
@@ -5,6 +5,7 @@
 // here is the API that we will need
 const math = require('mathjs');
 
+<<<<<<< Updated upstream
 // costs that we parse from specificiation
 const cost1 = math.parse('2*x^2');
 const cost2 = math.parse('x^2 + log(x, 2)');
@@ -61,10 +62,69 @@ function get_rec_def(recurance, conditions) {
   // Eg. f     (  n      ) = iff(  n      ,   n      >  1  , "f(n/2)+1",   1    )
   return name+'('+param+') = iff('+param+', '+param+'>='+c+', "'+expr+'", '+f_c+')';
 }
+=======
+// // costs that we parse from specificiation
+// const cost1 = math.parse('2*x^2');
+// const cost2 = math.parse('x^2 + log(x, 2)');
+//
+// // we manipulate the costs as we go on parsing the file
+// let poly1 = new math.OperatorNode('+', 'add', [cost1, cost2]);
+// let poly2 = new math.FunctionNode('max', [cost1, cost2]);
+//
+// // we can simplify equations every now and then to keep them compact
+// poly1 = math.simplify(poly1);
+// poly2 = math.simplify(poly2);
+//
+// // we can log things
+// console.log(poly1.toString());
+// console.log(poly2.toString());
+//
+// // when we are ready, we can evaluate
+// const compiledExpr = poly1.compile();
+// console.log(compiledExpr.evaluate({x: 1}));
+//
+// // we can add our own functions if we want
+// const f_n = math.parse('f(n/2) + 1'); // recurrance for binary search
+//
+// // we can traverse the expression to modify or analyze it!
+// const isRecursive = function (name, equation) {
+//   let recursive = false;
+//   // find out if the function is recursive: i.e. if name is used in equation
+//   equation.traverse(function (node, path, parent) {
+//     if (node.type === 'FunctionNode') {
+//       if (node.fn.toString() === name) {
+//         recursive = true;
+//       }
+//     }
+//   });
+//
+//   return recursive;
+// };
+// console.log('f(n) =', f_n.toString, ' is recursive:', isRecursive('f', f_n));
+
+
+// /*
+//  *  Define a recursive function type in mathjs
+//  */
+//
+// function get_rec_def(recurance, conditions) {
+//   const fn = math.parse(recurance);
+//   const name = fn.name;
+//   const param = fn.params[0];
+//   const expr = fn.expr;
+//
+//   const [fc, f_c] = math.parse(conditions.split('='));  // f(c)
+//   const c = fc.args[0];
+//
+//   // Eg. f     (    n    ) = iff(    n    ,     n    >=  1  , "f(n/2)+1",    0   )
+//   return name+'('+param+') = iff('+param+', '+param+'>='+c+', "'+expr+'", '+f_c+')';
+// }
+>>>>>>> Stashed changes
 
 // Set up the parser
 const parser = math.parser();
 parser.set('iff', function (n, above_base_case, thunk_f, base_val) {
+<<<<<<< Updated upstream
     parser.set('n', n);
     return above_base_case ? parser.evaluate(thunk_f) : base_val;
 });
@@ -72,6 +132,65 @@ parser.set('iff', function (n, above_base_case, thunk_f, base_val) {
 // g and f are supposed to be the same function
 const f = parser.evaluate(get_rec_def('h(n) = h(n/2)+1', 'h(1) = 0'));
 const g = parser.evaluate('g(n) = iff(n, n>1, "g(n/2)+1", 1)');
+=======
+  console.log('before', n, parser.scope.n);
+  parser.set('n', n);
+  console.log('after', n, parser.scope.n);
+  return above_base_case ? parser.evaluate(thunk_f) : base_val;
+});
 
-console.log(f(8));
+// g and f are supposed to be the same function
+// const f = parser.evaluate(get_rec_def('h(n) = h(n/2)+1', 'h(1) = 0'));
+const g = parser.evaluate('g(n) = iff(n, n>=1, "g(n/2)+1", 0)');
+>>>>>>> Stashed changes
+
+// console.log(f(8));
 console.log(g(8));
+
+// const fib = parser.evaluate(get_rec_def('f(n) = f(n-1) + f(n-2)', 'f(1) = 1'))
+
+
+
+
+// const evaluate = function (context, expression) {
+//   // Set up the parser with lazy evaluation and safe recursion
+//   const parser = math.parser();
+//   parser.set('iff', function (n, above_base_case, thunk_f, base_val) {
+//     parser.set('n', n);
+//     return above_base_case ? parser.evaluate(thunk_f) : base_val;
+//   });
+//
+//   if (Array.isArray(context)) {
+//     // Load all relavant functions into parser
+//     context.map(def => parser.evaluate(def));
+//   } else {
+//     parser.scope = context;
+//   }
+//
+//   return {
+//     value: parser.evaluate(expression),
+//     context: parser.getAll()
+//   };
+// }
+//
+// function get_rec_def(recurance, conditions) {
+//   const fn = math.parse(recurance);
+//   const name = fn.name;
+//   const param = fn.params[0];
+//   const expr = fn.expr;
+//
+//   const [fc, f_c] = math.parse(conditions.split('='));  // f(c)
+//   const c = fc.args[0];
+//
+//   // Eg. f     (    n    ) = iff(    n    ,     n    >=  1  , "f(n/2)+1",    0   )
+//   return name+'('+param+') = iff('+param+', '+param+'>='+c+', "'+expr+'", '+f_c+')';
+// }
+//
+//
+//
+// // g and f are the same function
+// const def_f = get_rec_def('f(n) = f(n/2)+1', 'f(1) = 0');
+// const def_g = 'g(n) = iff(n, n>=1, "g(n/2)+1", 0)';
+// const f_8 = evaluate([def_f, def_g], 'f(8)').value;
+//
+// console.log(f_8);;
---
+++ src/analyze/math.js
@@ -0,0 +1,97 @@
+const mathjs = require('mathjs');
+
+const ZERO = mathjs.parse('0');
+
+const emptyArgs = function (_arguments) {
+  return _arguments.length === 0 || _arguments[0] == null;
+};
+
+const operatorNode = function (operator, description, identity) {
+  return function () {
+    if (emptyArgs(arguments)) {
+      return identity;
+    }
+    if (arguments.length === 1) {
+      return arguments[0];
+    }
+    return new mathjs.OperatorNode(operator, description, Array.from(arguments));
+  };
+};
+
+const max = function () {
+  if (emptyArgs(arguments)) {
+    return ZERO;
+  }
+  if (arguments.length === 1) {
+    return arguments[0];
+  }
+  return new mathjs.FunctionNode('max', Array.from(arguments));
+};
+
+const iff = function (condition, ifVal, elseVal) {
+  return 'iff(' + condition.toString() + ',' + ifVal.toString() + ',' + elseVal.toString() +')';
+};
+
+const evaluate = function (context, expression) {
+  // Set up the parser with lazy evaluation and safe recursion
+  const parser = mathjs.parser();
+  parser.set('iff', function (n, above_base_case, thunk_f, base_val) {
+    parser.set('n', n);
+    return above_base_case ? parser.evaluate(thunk_f) : base_val;
+  });
+
+  if (Array.isArray(context)) {
+    // Load all relavant functions into parser
+    context.map(parser.evaluate);
+  } else {
+    parser.scope = context;
+  }
+
+  return {
+    value: parser.evaluate(expression),
+    context: parser.getAll()
+  };
+};
+
+module.exports = {
+  parse: mathjs.parse,
+  evaluate: evaluate,
+  ZERO: ZERO,
+  add: operatorNode('+', 'add', ZERO),
+  sub: operatorNode('-', 'subtract', ZERO),
+  multiply: operatorNode('*', 'multiply', ZERO),
+  div: operatorNode('/', 'divide', ZERO),
+  gt: operatorNode('>', 'larger', ZERO),
+  lt: operatorNode('<', 'smaller', ZERO),
+  gte: operatorNode('>=', 'largerEq', ZERO),
+  lte: operatorNode('<=', 'smallerEq', ZERO),
+  eq: operatorNode('==', 'equal', ZERO),
+  neq: operatorNode('!=', 'Unequal', ZERO),
+  not: operatorNode('not', 'not', ZERO),
+  max: max,
+  iff: iff
+};
+// https://mathjs.org/docs/expressions/syntax.html
+
+
+// For testing
+const get_rec_def = function (recurance, conditions) {
+  const fn = mathjs.parse(recurance);
+  const name = fn.name;
+  const param = fn.params[0];
+  const expr = fn.expr;
+
+  const [fc, f_c] = mathjs.parse(conditions.split('='));  // f(c)
+  const c = fc.args[0];
+
+  // Eg. f     (    n    ) = iff(    n    ,     n    >=  1  , "f(n/2)+1",    0   )
+  return name+'('+param+') = iff('+param+', '+param+'>='+c+', "'+expr+'", '+f_c+')';
+};
+const test_evaluate = function () {
+  // g and f are the same function
+  const def_f = get_rec_def('f(n) = f(n/2)+1', 'f(1) = 0');
+  const def_g = 'g(n) = iff(n, n>=1, "g(n/2)+1", 0)';
+  const f_8 = evaluate([def_f, def_g], 'f(8)').value;
+
+  console.log(f_8);
+};
