d6a11bd81d6547026684f2b2c137b45ec20dc853
Kinan Dak Al Bab <kinan.bab.14@gmail.com>
2020-03-05 00:14:14 -0500

    Implement OblivIf and Return

--- src/analyze/analysisVisitor/callAndReturn.js
+++ src/analyze/analysisVisitor/callAndReturn.js
@@ -1,5 +1,24 @@
-const ReturnStatement = function (node) {};
-const FunctionCall = function (node) {};
+const ReturnStatement = function (node, pathStr) {
+  const childResult = this.visit(node, pathStr + '[returnExpression]');
+
+  // Return statement not allowed in typings and costs: skip!
+  const metricAgg = this.analyzer.metric.aggregateReturnStatement(node, childResult.type, childResult.metric);
+
+  return {
+    type: childResult.type,
+    metric: metricAgg
+  };
+};
+
+const FunctionCall = function (node, pathStr) {
+  const func = node.function;
+  const parameters = node.parameters;
+
+  // Two cases: function is a name or a dotExpression
+  let abstractionType, abstractionMetric;
+  if (func.nodeType === 'NameExpression') {
+  }
+};
 
 module.exports = {
   ReturnStatement: ReturnStatement,
--- src/analyze/analysisVisitor/oblivIf.js
+++ src/analyze/analysisVisitor/oblivIf.js
@@ -1,4 +1,8 @@
-const OblivIf = function (node, args) {};
+const If = require('./if.js');
+
+const OblivIf = function (node, pathStr) {
+  return If.If(node, pathStr + 'obliv');
+};
 
 module.exports = {
   OblivIf: OblivIf
